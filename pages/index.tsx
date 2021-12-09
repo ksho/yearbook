cfd2720471d5403e412cc1260a46ef6a4df01d7a
Karl Shouler <kshouler@gmail.com>
2021-12-08 21:09:43 -0500

    s3 test

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,9 @@ import Image from 'next/image'
 import '../styles/Home.module.css'
 import styled from 'styled-components';
 
+// import fs from 'fs'
+// import path from 'path'
+
 import Masonry from 'react-masonry-css'
 
 import { Cloudinary } from '@cloudinary/url-gen';
@@ -26,29 +29,78 @@ const cld = new Cloudinary({
     cloudName: 'kshoyearbook'
   }
 });
+import aws from 'aws-sdk';
 
-// export async function getStaticProps() {
+export async function getStaticProps() {
 // export const getServerSideProps: GetServerSideProps = async (context) => {
-//   const res = await fetch('https://res.cloudinary.com/kshoyearbook/image/list/yearbook_2021.json')
-//   const data = await res.json()
+  // const res = await fetch('https://res.cloudinary.com/kshoyearbook/image/list/yearbook_2021.json')
+  // const json = await res.json()
+  
 
-//   if (!data) {
-//     return {
-//       notFound: true,
-//     }
-//   }
+  // if (!json) {
+  //   return {
+  //     notFound: true,
+  //   }
+  // }
 
-//   return {
-//     props: { data }, // will be passed to the page component as props
-//   }
-// }
+  // const data = json.resources
+
+  // return {
+  //   props: { data }, // will be passed to the page component as props
+  // }
+
+  
+  // const s3 = new AWS.S3({
+  //   credentials: {
+  //     accessKeyId: ,
+  //     secretAccessKey: 
+  //   }
+  // });
+
+  aws.config.update({
+    accessKeyId: process.env.AWS_S3_SECRET,
+    secretAccessKey: process.env.AWS_S3_ACCESS_KEY,
+    region: 'us-east-1',
+    signatureVersion: 'v4',
+  });
+
+  const s3 = new aws.S3();
+
+  // The id from the route (e.g. /img/abc123987)
+  // let filename = query.id;
+
+  const params = {
+    Bucket: 'ksho-share',
+    Prefix: '2021',
+    // Key: 'test.jpg'
+  };
+
+  const res = await new Promise((resolve, reject) => {
+    s3.listObjectsV2(params, (err, data) => {
+      if (err) reject(err);
+      console.log(data);
+      const keys = data.Contents.map((c) => c.Key)
+      keys.shift();
+      // let imgData = 'data:image/jpeg;base64,' + data.Body.toString('base64');
+      resolve(keys);
+    });
+  });
+
+  return {props: { data: res } };
+
+}
 
 const ImageList = (images: any) => {
+  
   
   const imageList = images.map((i: string, index: number) => {
     return (
       <ImageWrapper key={index}>
-        <img src={i} width="100%" />
+        <img src={`https://ksho-share.s3.amazonaws.com/${i}`} width="100%" />
+        {/* <AdvancedImage
+          cldImg={cld.image(i.public_id) }
+          plugins={[lazyload(), responsive(100), placeholder()]}
+        /> */}
       </ImageWrapper>
       
       
@@ -59,15 +111,37 @@ const ImageList = (images: any) => {
   return imageList;
 };
 
-function Home() {
 
-  const fetcher = (url: string) => fetch(url).then((res) => res.json());
-  const { data, error } = useSWR('/api/readphotos', fetcher);
+
+// function getImages() {
+//   const dirRelativeToPublicFolder = 'images/photos/2021';
+
+//   const dir = path.resolve('./public', dirRelativeToPublicFolder);
+
+//   // const dir = path.resolve('./public/images/photos/2021');
+
+//   const filenames = fs.readdirSync(dir);
+
+//   const images = filenames.map(name => path.join('/', dirRelativeToPublicFolder, name))
+
+//   return images;
+// }
+
+function Home(data) {
+
+  // Uncomment for local files
+  // const fetcher = (url: string) => fetch(url).then((res) => res.json());
+  // const { images, error } = useSWR('/api/readphotos', fetcher);
+  const images = data.data;
   console.log(data)
 
-  if (error) return <div>failed to load</div>
-  if (!data) return <div>loading...</div>
 
+  // Uncomment for cloudinary
+  // const images = data.data
+
+  // if (error) return <div>failed to load</div>
+  if (!images) return <div>loading...</div>
+  console.log("images", images)
 
 
   // EXIF stuff .. make this async
@@ -92,7 +166,7 @@ function Home() {
       className="my-masonry-grid"
       columnClassName="my-masonry-grid_column"
     >
-      {ImageList(data)}
+      {ImageList(images)}
     </Masonry>
     // </div>
 
